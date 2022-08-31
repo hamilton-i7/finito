7b23f02b0f1bd456a8e31cdaab0d7190fcef3bcf
Juan Hamilton <hedwards507@gmail.com>
2022-08-31 03:58:27 -0500

    Add FindOne use case and adapt tests

--- app/src/test/java/com/example/finito/features/boards/data/repository/FakeBoardRepository.kt
+++ app/src/test/java/com/example/finito/features/boards/data/repository/FakeBoardRepository.kt
@@ -5,6 +5,9 @@ import com.example.finito.features.boards.domain.entity.BoardWithLabels
 import com.example.finito.features.boards.domain.entity.DetailedBoard
 import com.example.finito.features.boards.domain.entity.SimpleBoard
 import com.example.finito.features.boards.domain.repository.BoardRepository
+import com.example.finito.features.subtasks.domain.entity.Subtask
+import com.example.finito.features.tasks.domain.entity.Task
+import com.example.finito.features.tasks.domain.entity.TaskWithSubtasks
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.flow
 
@@ -43,7 +46,44 @@ class FakeBoardRepository : BoardRepository {
     }
 
     override suspend fun findOne(id: Int): DetailedBoard? {
-        TODO("Not yet implemented")
+        val board = boards.find { it.board.boardId == id }
+        return board?.let {
+            DetailedBoard(
+                boardId = it.board.boardId,
+                name = it.board.name,
+                createdAt = it.board.createdAt,
+                tasks = listOf(
+                    TaskWithSubtasks(
+                        task = Task(
+                            name = "Task name",
+                            boardId = it.board.boardId,
+                            position = 0
+                        ),
+                        subtasks = emptyList(),
+                    ),
+                    TaskWithSubtasks(
+                        task = Task(
+                            taskId = 1,
+                            name = "Task name",
+                            boardId = it.board.boardId,
+                            position = 1
+                        ),
+                        subtasks = listOf(
+                            Subtask(
+                                name = "Subtask name",
+                                taskId = 1,
+                                position = 0
+                            ),
+                            Subtask(
+                                name = "Subtask name",
+                                taskId = 1,
+                                position = 1
+                            ),
+                        ),
+                    ),
+                )
+            )
+        }
     }
 
     override suspend fun findNewestId(): Int {

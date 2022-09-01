13b182baab10beba9cea9149b86a759f5ec8d958
Juan Hamilton <hedwards507@gmail.com>
2022-08-31 22:00:36 -0500

    Remove filtering functionality

--- app/src/main/java/com/example/finito/features/boards/domain/usecase/FindDeletedBoards.kt
+++ app/src/main/java/com/example/finito/features/boards/domain/usecase/FindDeletedBoards.kt
@@ -2,36 +2,15 @@ package com.example.finito.features.boards.domain.usecase
 
 import com.example.finito.features.boards.domain.entity.BoardWithLabels
 import com.example.finito.features.boards.domain.repository.BoardRepository
-import com.example.finito.features.boards.domain.util.BoardOrder
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 
 class FindDeletedBoards(
     private val repository: BoardRepository
 ) {
-    operator fun invoke(
-        boardOrder: BoardOrder = BoardOrder.A_Z,
-        vararg labelIds: Int,
-    ): Flow<List<BoardWithLabels>> {
+    operator fun invoke(): Flow<List<BoardWithLabels>> {
         return repository.findDeletedBoards().map { boards ->
-            if (labelIds.isEmpty()) {
-                return@map sortBoards(boardOrder, boards)
-            }
-            val idsMap = labelIds.groupBy { it }
-            val filteredBoards = boards.filter { board ->
-                board.labels.any { idsMap[it.labelId] != null }
-            }
-            sortBoards(boardOrder, filteredBoards)
+            boards.sortedBy { it.board.normalizedName }
         }
-    }
-
-    private fun sortBoards(
-        boardOrder: BoardOrder,
-        boards: List<BoardWithLabels>,
-    ): List<BoardWithLabels> = when(boardOrder) {
-        BoardOrder.A_Z -> boards.sortedBy { it.board.normalizedName }
-        BoardOrder.Z_A -> boards.sortedByDescending { it.board.normalizedName }
-        BoardOrder.NEWEST -> boards.sortedByDescending { it.board.createdAt }
-        BoardOrder.OLDEST -> boards.sortedBy { it.board.createdAt }
     }
 }
--- app/src/test/java/com/example/finito/features/boards/domain/usecase/FindDeletedBoardsTest.kt
+++ app/src/test/java/com/example/finito/features/boards/domain/usecase/FindDeletedBoardsTest.kt
@@ -3,7 +3,6 @@ package com.example.finito.features.boards.domain.usecase
 import com.example.finito.features.boards.data.repository.FakeBoardRepository
 import com.example.finito.features.boards.domain.entity.Board
 import com.example.finito.features.boards.domain.entity.BoardWithLabels
-import com.example.finito.features.boards.domain.util.BoardOrder
 import com.example.finito.features.labels.domain.entity.SimpleLabel
 import com.google.common.truth.Truth.assertThat
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -55,83 +54,12 @@ class FindDeletedBoardsTest {
     }
 
     @Test
-    fun `findDeletedBoards( returns list sorted by name ascending`(): Unit = runTest {
-        val sortedBoards = findDeletedBoards(BoardOrder.A_Z).first()
+    fun `findDeletedBoards( returns list sorted by name ascending`() = runTest {
+        val sortedBoards = findDeletedBoards().first()
 
         for (i in 0..sortedBoards.size - 2) {
             assertThat(sortedBoards[i].board.normalizedName)
                 .isLessThan(sortedBoards[i+1].board.normalizedName)
         }
-    }
-
-    @Test
-    fun `findDeletedBoards( returns list sorted by name descending`(): Unit = runTest {
-        val sortedBoards = findDeletedBoards(BoardOrder.Z_A).first()
-
-        for (i in 0..sortedBoards.size - 2) {
-            assertThat(sortedBoards[i].board.normalizedName)
-                .isGreaterThan(sortedBoards[i+1].board.normalizedName)
-        }
-    }
-
-    @Test
-    fun `findDeletedBoards( returns list sorted by creation date ascending`(): Unit = runTest {
-        val sortedBoards = findDeletedBoards(BoardOrder.OLDEST).first()
-
-        for (i in 0..sortedBoards.size - 2) {
-            assertThat(
-                sortedBoards[i].board.createdAt.isBefore(sortedBoards[i+1].board.createdAt)
-            ).isTrue()
-        }
-    }
-
-    @Test
-    fun `findDeletedBoards( returns list sorted by creation date descending`(): Unit = runTest {
-        val sortedBoards = findDeletedBoards(BoardOrder.NEWEST).first()
-
-        for (i in 0..sortedBoards.size - 2) {
-            assertThat(
-                sortedBoards[i].board.createdAt.isAfter(sortedBoards[i+1].board.createdAt)
-            ).isTrue()
-        }
-    }
-
-    @Test
-    fun `find deleted boards returns filtered list`() = runTest {
-        val deleteLabeledBoards = dummyBoards.filter {
-            it.labels.isNotEmpty() && it.board.deleted
-        }
-        val boards = findDeletedBoards().first()
-        val filteredBoards = findDeletedBoards(
-            labelIds = deleteLabeledBoards.flatMap { it.labels }.map { it.labelId }.toIntArray()
-        ).first()
-
-        assertThat(boards).isNotEmpty()
-        assertThat(deleteLabeledBoards).isNotEmpty()
-        assertThat(filteredBoards).isNotEmpty()
-        assertThat(deleteLabeledBoards.size).isEqualTo(filteredBoards.size)
-        assertThat(boards.size).isGreaterThan(filteredBoards.size)
-        assertThat(filteredBoards.all { it.board.deleted }).isTrue()
-
-        val filteredBoards2 = findDeletedBoards(
-            labelIds = deleteLabeledBoards.flatMap {
-                it.labels
-            }.take(1).map { it.labelId }.toIntArray()
-        ).first()
-        assertThat(filteredBoards2).isNotEmpty()
-        assertThat(boards.size).isGreaterThan(filteredBoards2.size)
-        assertThat(filteredBoards.size).isGreaterThan(filteredBoards2.size)
-
-        val filteredBoards3 = findDeletedBoards(labelIds = intArrayOf()).first()
-        assertThat(filteredBoards3.size).isEqualTo(boards.size)
-
-        val activeLabeledBoards = dummyBoards.filter {
-            it.labels.isNotEmpty() && !it.board.deleted && !it.board.archived
-        }
-        val filteredBoards5 = findDeletedBoards(
-            labelIds = activeLabeledBoards.flatMap { it.labels }.map { it.labelId }.toIntArray()
-        ).first()
-        assertThat(activeLabeledBoards).isNotEmpty()
-        assertThat(filteredBoards5).isEmpty()
     }
 }

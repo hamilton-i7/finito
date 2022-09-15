b88655fa71bc54a11385b6a0e36e7062537af6e3
Juan Hamilton <hedwards507@gmail.com>
2022-09-15 02:35:07 -0500

    Setup Label screen

---
+++ app/src/main/java/com/example/finito/features/labels/presentation/screen/label/LabelViewModel.kt
@@ -0,0 +1,203 @@
+package com.example.finito.features.labels.presentation.screen.label
+
+import android.content.SharedPreferences
+import androidx.annotation.StringRes
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.SavedStateHandle
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.finito.R
+import com.example.finito.core.di.PreferencesModule
+import com.example.finito.core.domain.util.SEARCH_DELAY_MILLIS
+import com.example.finito.core.domain.util.SortingOption
+import com.example.finito.core.presentation.Screen
+import com.example.finito.features.boards.domain.entity.BoardState
+import com.example.finito.features.boards.domain.entity.BoardWithLabelsAndTasks
+import com.example.finito.features.boards.domain.usecase.BoardUseCases
+import com.example.finito.features.boards.utils.DeactivateMode
+import com.example.finito.features.labels.domain.entity.Label
+import com.example.finito.features.labels.domain.usecase.LabelUseCases
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
+import java.time.LocalDateTime
+import javax.inject.Inject
+
+@HiltViewModel
+class LabelViewModel @Inject constructor(
+    private val boardUseCases: BoardUseCases,
+    private val labelUseCases: LabelUseCases,
+    private val preferences: SharedPreferences,
+    private val savedStateHandle: SavedStateHandle,
+) : ViewModel() {
+
+    var label by mutableStateOf<Label?>(null)
+        private set
+
+    var boards by mutableStateOf<List<BoardWithLabelsAndTasks>>(emptyList())
+        private set
+
+    private var fetchBoardsJob: Job? = null
+
+    var showSearchBar by mutableStateOf(false)
+        private set
+
+    var searchQuery by mutableStateOf("")
+        private set
+
+    private var searchJob: Job? = null
+
+    var boardsOrder by mutableStateOf(
+        preferences.getString(
+            PreferencesModule.TAG.BOARDS_ORDER.name,
+            SortingOption.Common.Default.name
+        )?.let {
+            when (it) {
+                SortingOption.Common.NameZA.name -> SortingOption.Common.NameZA
+                SortingOption.Common.Newest.name -> SortingOption.Common.Newest
+                SortingOption.Common.Oldest.name -> SortingOption.Common.Oldest
+                else -> SortingOption.Common.NameAZ
+            }
+        } ?: SortingOption.Common.Default
+    ); private set
+
+    var gridLayout by mutableStateOf(preferences.getBoolean(
+        PreferencesModule.TAG.GRID_LAYOUT.name,
+        true
+    )); private set
+
+    private var recentlyDeactivatedBoard: BoardWithLabelsAndTasks? = null
+
+    var showCardMenu by mutableStateOf(false)
+
+    var selectedBoardId by mutableStateOf(0)
+        private set
+
+    private val _eventFlow = MutableSharedFlow<Event>()
+    val eventFlow = _eventFlow.asSharedFlow()
+
+    init {
+        fetchLabel()
+    }
+
+    fun onEvent(event: LabelEvent) {
+        when (event) {
+            LabelEvent.RenameLabel -> TODO()
+            LabelEvent.DeleteLabel -> TODO()
+            is LabelEvent.ArchiveBoard -> onDeactivateBoard(event.board, DeactivateMode.ARCHIVE)
+            is LabelEvent.MoveBoardToTrash -> onDeactivateBoard(event.board, DeactivateMode.DELETE)
+            LabelEvent.RestoreBoard -> onRestoreBoard()
+            is LabelEvent.SearchBoards -> onSearchBoards(event.query)
+            is LabelEvent.ShowCardMenu -> onShowCardMenu(id = event.boardId, show = event.show)
+            is LabelEvent.ShowSearchBar -> onShowSearchBar(event.show)
+            is LabelEvent.SortBoards -> onSortBoards(event.sortingOption)
+            LabelEvent.ToggleLayout -> onToggleLayout()
+        }
+    }
+
+    private fun onSortBoards(sortingOption: SortingOption.Common) {
+        if (sortingOption::class == boardsOrder::class) return
+
+        boardsOrder = sortingOption
+        with(preferences.edit()) {
+            putString(PreferencesModule.TAG.BOARDS_ORDER.name, sortingOption.name)
+            apply()
+        }
+        fetchBoards()
+    }
+
+    private fun onRestoreBoard() = viewModelScope.launch {
+        recentlyDeactivatedBoard?.let {
+            boardUseCases.updateBoard(
+                it.copy(board = it.board.copy(state = BoardState.ACTIVE, removedAt = null))
+            )
+            recentlyDeactivatedBoard = null
+        }
+    }
+
+    private fun onDeactivateBoard(
+        board: BoardWithLabelsAndTasks,
+        mode: DeactivateMode,
+    ) = viewModelScope.launch {
+        with(board) {
+            when (mode) {
+                DeactivateMode.ARCHIVE -> {
+                    boardUseCases.updateBoard(copy(board = this.board.copy(state = BoardState.ARCHIVED)))
+                    _eventFlow.emit(Event.ShowSnackbar(message = R.string.board_archived))
+                }
+                DeactivateMode.DELETE -> {
+                    boardUseCases.updateBoard(
+                        copy(
+                            board = this.board.copy(
+                                state = BoardState.DELETED, removedAt = LocalDateTime.now()
+                            )
+                        )
+                    )
+                    _eventFlow.emit(Event.ShowSnackbar(message = R.string.board_moved_to_trash))
+                }
+            }
+            recentlyDeactivatedBoard = this
+        }
+    }
+
+    private fun onShowCardMenu(id: Int, show: Boolean) {
+        selectedBoardId = if (!show) 0 else id
+        showCardMenu = show
+    }
+
+    private fun onSearchBoards(query: String) {
+        searchQuery = query
+        searchJob?.cancel()
+        searchJob = viewModelScope.launch {
+            delay(SEARCH_DELAY_MILLIS)
+            fetchBoards()
+        }
+    }
+
+    private fun onShowSearchBar(show: Boolean) {
+        if (!show) {
+            searchQuery = ""
+            fetchBoards()
+        }
+        showSearchBar = show
+    }
+
+    private fun onToggleLayout() {
+        gridLayout = !gridLayout
+        with(preferences.edit()) {
+            putBoolean(PreferencesModule.TAG.GRID_LAYOUT.name, gridLayout)
+            apply()
+        }
+    }
+
+    private fun fetchLabel() {
+        savedStateHandle.get<Int>(Screen.LABEL_ROUTE_ARGUMENT)?.let { labelId ->
+            viewModelScope.launch {
+                labelUseCases.findLabel(labelId).let { label = it }.also { fetchBoards() }
+            }
+        }
+    }
+
+    private fun fetchBoards() = viewModelScope.launch {
+        fetchBoardsJob?.cancel()
+        fetchBoardsJob = boardUseCases.findActiveBoards(
+            boardOrder = boardsOrder,
+            searchQuery = searchQuery,
+            label!!.labelId
+        ).onEach { boards ->
+            this@LabelViewModel.boards = boards
+            boardsOrder = boardsOrder
+        }.launchIn(viewModelScope)
+    }
+
+    sealed class Event {
+        data class ShowSnackbar(@StringRes val message: Int) : Event()
+    }
+}

ede9f0637bddbae103b7bae997eb71429ebb6bbd
Juan Hamilton <hedwards507@gmail.com>
2022-10-01 06:15:54 -0500

    Remove unused code

--- app/src/main/java/com/example/finito/core/presentation/util/DialogType.kt
+++
@@ -1,9 +0,0 @@
-package com.example.finito.core.presentation.util
-
-import com.example.finito.features.boards.domain.entity.Board
-
-sealed class DialogType {
-    data class DeleteBoard(val board: Board) : DialogType()
-
-    object EmptyTrash : DialogType()
-}

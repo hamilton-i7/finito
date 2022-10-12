07db254c66b58d65799288a7958c043d8419024f
Juan Hamilton <hedwards507@gmail.com>
2022-10-12 02:08:08 -0500

    Change Task schema

--- app/src/main/java/com/example/finito/features/tasks/domain/entity/Task.kt
+++ app/src/main/java/com/example/finito/features/tasks/domain/entity/Task.kt
@@ -2,10 +2,9 @@ package com.example.finito.features.tasks.domain.entity
 
 import android.os.Parcelable
 import androidx.room.*
-import com.example.finito.core.domain.Priority
-import com.example.finito.core.domain.Reminder
 import com.example.finito.core.domain.util.normalize
 import com.example.finito.features.boards.domain.entity.Board
+import com.example.finito.features.tasks.domain.util.Priority
 import kotlinx.parcelize.Parcelize
 import java.time.LocalDate
 import java.time.LocalDateTime
@@ -39,7 +38,6 @@ data class Task(
     @ColumnInfo(defaultValue = "0") val completed: Boolean = false,
     @ColumnInfo(defaultValue = "NULL") val date: LocalDate? = null,
     @ColumnInfo(defaultValue = "NULL") val time: LocalTime? = null,
-    @ColumnInfo(defaultValue = "NULL") val reminder: Reminder? = null,
     @ColumnInfo(defaultValue = "NULL") val priority: Priority? = null,
     @ColumnInfo(name = "board_position", defaultValue = "NULL") val boardPosition: Int? = null,
     @ColumnInfo(name = "completed_at", defaultValue = "NULL")
@@ -58,7 +56,6 @@ data class Task(
                 date = if (index % 3 == 0) LocalDate.now().plusDays((0..100).random().toLong()) else null,
                 time = if (index % 6 == 0) LocalTime.now().plusHours((0..200).random().toLong()) else null,
                 completedAt = if (index % 5 == 0) LocalDateTime.now().plusMinutes((0..1_500).random().toLong()) else null,
-                reminder = if (index % 7 == 0) enumValues<Reminder>().random() else null,
                 priority = if (index % 9 == 0) enumValues<Priority>().random() else null,
             )
         }.shuffled()
